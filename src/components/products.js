a9f7f943a232b5a721c750de8fe7bc6edd364c5a
Martina <rozzel03@email.franklin.edo>
2019-08-05 20:03:50 -0400

    products addition

--- src/components/products.js
+++ src/components/products.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Grid, Cell} from 'react-mdl';
 import productImage from './img/blade1.jpg';
-import productImage1 from './img/windshield.jpg';
+import productImage1 from './img/Windshield.jpg';
 
 
 class Products extends Component {
@@ -16,49 +16,158 @@ class Products extends Component {
                         <img src={productImage1} alt="productImage1"  className="product-img"/>
                         <div className="products-content">DW02418 GTY</div>
                         <div className="products-content">Domestic Windshield WINDSHIELD GREEN TINT SOLAR ACOUSTIC INTERLAYER 3RD VISOR FRIT AFTERMARKET</div>
-                        <div className="products-content">$859.00</div>
+                        <div className="products-content">List Price: $859.00</div>
+                        <div className="products-content">Your Price: $145.91</div>
                     </Cell>
                     <Cell col={4}>
-                        <div className="products-content">Product Name</div>
+                        <img src={productImage1} alt="productImage1"  className="product-img"/>
+                        <div className="products-content">DW02153 GTY</div>
+                        <div className="products-content">Domestic Windshield WINDSHIELD GREEN TINT SOLAR 3RD VISOR FRIT AFTERMARKET</div>
+                        <div className="products-content">List Price: $438.05</div>
+                        <div className="products-content">Your Price: $151.83</div>
                     </Cell>
                     <Cell col={4}>
-                        <div className="products-content">Product Name</div>
+                        <img src={productImage1} alt="productImage1"  className="product-img"/>
+                        <div className="products-content">DW02355 GTY</div>
+                        <div className="products-content">Domestic Windshield WINDSHIELD GREEN TINT RAIN SENSOR SOLAR ACOUSTIC INTERLAYER LDWS 3RD VISOR FRIT</div>
+                        <div className="products-content">List Price: $625.65</div>
+                        <div className="products-content">Your Price: $151.83</div>
                     </Cell>
                     <Cell col={4}>
-                        <div className="products-content">Product Name</div>
+                        <img src={productImage1} alt="productImage1"  className="product-img"/>
+                        <div className="products-content">DW02154 GTY</div>
+                        <div className="products-content">Domestic Windshield WINDSHIELD GREEN TINT SOLAR 3RD VISOR FRIT ACOUSTIC INTERLAYER AFTERMARKET</div>
+                        <div className="products-content">List Price: $708.45</div>
+                        <div className="products-content">Your Price: $409.99</div>
                     </Cell>
                     <Cell col={4}>
-                        <div className="products-content">Product Name</div>
+                        <img src={productImage1} alt="productImage1"  className="product-img"/>
+                        <div className="products-content">DW02130 GTY</div>
+                        <div className="products-content">Domestic Windshield WINDSHIELD GREEN TINT SOLAR FORD OVAL IN 3RD VISOR FR</div>
+                        <div className="products-content">List Price: $508.45</div>
+                        <div className="products-content">Your Price: $135.99</div>
                     </Cell>
                     <Cell col={4}>
-                        <div className="products-content">Product Name</div>
+                        <img src={productImage1} alt="productImage1"  className="product-img"/>
+                        <div className="products-content">DW02130 GTYOEM</div>
+                        <div className="products-content">Domestic Windshield WINDSHIELD GREEN TINT SOLAR FORD OVAL IN 3RD VISOR FR - OEM</div>
+                        <div className="products-content">List Price: $808.45</div>
+                        <div className="products-content">Your Price: $309.99</div>
+                    </Cell>
+                    <Cell col={4}>
+                        <img src={productImage1} alt="productImage1"  className="product-img"/>
+                        <div className="products-content">DW02354 GTYOEM</div>
+                        <div className="products-content">Domestic Windshield WINDSHIELD GREEN TINT SOLAR ACOUSTIC INTERLAYER FORD OVAL IN 3RD VISOR FR - OEM</div>
+                        <div className="products-content">List Price: $757.55</div>
+                        <div className="products-content">Your Price: $314.89</div>
+                    </Cell>
+                    <Cell col={4}>
+                        <img src={productImage1} alt="productImage1"  className="product-img"/>
+                        <div className="products-content">DW02131 GTY</div>
+                        <div className="products-content">Domestic Windshield WINDSHIELD GREEN TINT SOLAR ACOUSTIC INTERLAYER FORD OVAL IN 3RD VISOR FR</div>
+                        <div className="products-content">List Price: $957.55</div>
+                        <div className="products-content">Your Price: $514.89</div>
+                    </Cell>
+                    <Cell col={4}>
+                        <img src={productImage1} alt="productImage1"  className="product-img"/>
+                        <div className="products-content">DW02366 GTY</div>
+                        <div className="products-content">Domestic Windshield WINDSHIELD GREEN TINT SOLAR ACOUSTIC INTERLAYER 3RD VISOR FRIT LDWS HEATED WIPER PARK W/COMBINATION BRACKET</div>
+                        <div className="products-content">List Price: $949.30</div>
+                        <div className="products-content">Your Price: $426.93</div>
                     </Cell>
                     <Cell col={12}>
                         <div className="products-title">Wipers</div>
                     </Cell>
                     <Cell col={4}>
-                        <div className="products-content">Product Name</div>
                         <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB11</div>
+                        <div className="products-content">WIPER STANDARD BLADE 11</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
                     </Cell>
                     <Cell col={4}>
-                        <div className="products-content">Product Name</div>
                         <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB13</div>
+                        <div className="products-content">WIPER STANDARD BLADE 13</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
                     </Cell>
                     <Cell col={4}>
-                        <div className="products-content">Product Name</div>
                         <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB15</div>
+                        <div className="products-content">WIPER STANDARD BLADE 16</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
                     </Cell>
                     <Cell col={4}>
-                        <div className="products-content">Product Name</div>
-                        <img src={productImage} alt="productImage"  className="product-img"/>
+                    <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB16</div>
+                        <div className="products-content">WIPER STANDARD BLADE 16</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
                     </Cell>
                     <Cell col={4}>
-                        <div className="products-content">Product Name</div>
                         <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB17</div>
+                        <div className="products-content">WIPER STANDARD BLADE 17</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
                     </Cell>
                     <Cell col={4}>
-                        <div className="products-content">Product Name</div>
                         <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB18</div>
+                        <div className="products-content">WIPER STANDARD BLADE 18</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
+                    </Cell>
+                    <Cell col={4}>
+                        <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB19</div>
+                        <div className="products-content">WIPER STANDARD BLADE 19</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
+                    </Cell>
+                    <Cell col={4}>
+                        <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB20</div>
+                        <div className="products-content">WIPER STANDARD BLADE 20</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
+                    </Cell>
+                    <Cell col={4}>
+                        <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB21</div>
+                        <div className="products-content">WIPER STANDARD BLADE 21</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
+                    </Cell>
+                    <Cell col={4}>
+                        <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB22</div>
+                        <div className="products-content">WIPER STANDARD BLADE 22</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
+                    </Cell>
+                    <Cell col={4}>
+                        <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB24</div>
+                        <div className="products-content">WIPER STANDARD BLADE 24</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
+                    </Cell>
+                    <Cell col={4}>
+                        <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB26</div>
+                        <div className="products-content">WIPER STANDARD BLADE 26</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
+                    </Cell>
+                    <Cell col={4}>
+                        <img src={productImage} alt="productImage"  className="product-img"/>
+                        <div className="products-content">SB28</div>
+                        <div className="products-content">WIPER STANDARD BLADE 28</div>
+                        <div className="products-content">List Price: $15.00</div>
+                        <div className="products-content">Your Price: $5.99</div>
                     </Cell>
                 </Grid>
             </div>
